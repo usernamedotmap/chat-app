5d64a8ad60e78fb7f7fe9d36e5a8861d585d74cf
usernamedotmap <benasjoshuac@gmail.com>
2024-11-26 21:45:38 +0800

    lagay lang ng sounds

--- front-end/src/hooks/useListenMessage.js
+++ front-end/src/hooks/useListenMessage.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
+import soundsFile from '../assets/sounds/sounds.mp3'
 
 
 const useListenMessage = () => {
@@ -10,6 +11,8 @@ const useListenMessage = () => {
     useEffect(() => {
         socket.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true;
+            const sounds = new Audio(soundsFile);
+            sounds.play();
             setMessage([...messages, newMessage])
         })
 
